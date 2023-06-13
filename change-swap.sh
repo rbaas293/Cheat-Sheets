0f68ca576f40db8dc45c7059cdfaa170a6561980
Ryan Baas <rybaas293@gmail.com>
2023-06-12 20:37:20 -0400

    Add files via upload

---
+++ change-swap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+SIZE=$1
+
+re='^[0-9]+$'
+if ! [[ $SIZE =~ $re ]] ; then
+	echo "error: Not a number ($SIZE)" >&2; exit 1
+fi
+
+echo "New Swap size will be: $SIZE"
+
+sudo swapoff -a
+
+sudo fallocate -l "${SIZE}G" /swapfile
+
+sudo chmod 600 /swapfile
+
+sudo mkswap /swapfile
+
+sudo swapon /swapfile
+
+echo "Done!"
